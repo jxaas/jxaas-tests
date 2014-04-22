a7a14f7dc7f635c34fefa3201516b8a0f32446f8
Justin SB <justin@fathomdb.com>
2014-04-22 09:36:47 -0700

    Make tests derive from common test class

---
+++ test-mysql.py
@@ -0,0 +1,97 @@
+import functools
+import os
+import json
+import subprocess
+import sys
+import time
+import yaml
+import jujuxaas.client
+
+from utils import *
+
+import logging
+log = logging.getLogger(__name__)
+
+import testbase
+
+import MySQLdb
+
+class TestMysql(testbase.TestBase):
+  def __init__(self):
+    super(TestMysql, self).__init__()
+    self.bundle_type = 'mysql'
+    self.proxy_charm = 'cs:~justin-fathomdb/trusty/mysql-proxy'
+    self.consumer_charm = 'cs:~justin-fathomdb/trusty/mediawiki'
+    self.juju_interface = 'mysql'
+
+  def mysql_connect(self, relinfo):
+    db = MySQLdb.connect(host=relinfo.get('host'),
+                         user=relinfo.get('user'),
+                         passwd=relinfo.get('password'),
+                         db=relinfo.get('database'))
+    return db
+
+  def execute_sql(self, db, sql):
+    log.info("Running SQL: %s", sql)
+    cur = db.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+  def create_consumer_relation(self):
+    juju_ensure_relation('%s:db' % self.service_name, '%s:db' % self.consumer_service_name)
+
+  def check_for_wiki_tables(self, relinfo):
+    db = self.mysql_connect(relinfo)
+    rows = self.execute_sql(db, 'SHOW TABLES')
+    tables = [row[0] for row in rows]
+
+    if 'interwiki' in tables:
+      return tables
+
+    return None
+
+  def check_service_state_consumer(self):
+    relinfo = self.get_relation_properties()
+    # Wait for consumer charm to finish configuration
+    wait_for(functools.partial(self.check_for_wiki_tables, relinfo))
+
+    self.run_test1(relinfo)
+
+  def change_properties(self):
+    properties = juju_get_properties(self.backend_main_service_name)
+    if str(properties['slow-query-time']['value']) != '0.01':
+      log.info("Setting slow-query-time property on proxy charm; should be forwarded to main charm")
+      juju_set_property(self.service_name, 'slow-query-time', '0.01')
+
+      time.sleep(10)
+
+      properties = juju_get_properties(self.backend_main_service_name)
+      log.info("Properties = %s", properties)
+      log.info("slow-query-time = %s", properties['slow-query-time'])
+      assert '0.01' == str(properties['slow-query-time']['value'])
+
+# def test_scaling(self):
+# TODO: Scaling doesn't make sense with the current MySQL charm
+# TODO: Should we use MySQL-Proxy as a charm on the server side?
+# jxaas.ensure_instance(tenant, bundle_type, service_name, units=2)
+
+  def run_test1(self, relinfo):
+    db = self.mysql_connect(relinfo)
+    self.execute_sql(db, 'CREATE TABLE IF NOT EXISTS test1 (id int)')
+
+    self.execute_sql(db, 'DELETE FROM test1')
+    for i in xrange(10):
+      self.execute_sql(db, 'INSERT INTO test1 VALUES (1)')
+
+    for i in xrange(12):
+      self.execute_sql(db, 'INSERT INTO test1 SELECT * FROM test1')
+
+    rows = self.execute_sql(db, 'SELECT * FROM test1')
+    log.info("Rows %s", len(rows))
+    assert len(rows) > 40000
+
+
+t = TestMysql()
+t.run_test()
---
+++ testbase.py
@@ -0,0 +1,141 @@
+import functools
+import os
+import json
+import subprocess
+import sys
+import time
+import yaml
+import jujuxaas.client
+
+from utils import *
+
+import logging
+log = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class TestBase(object):
+  def __init__(self, prefix=None):
+    if not prefix:
+      if len(sys.argv) > 1:
+        prefix = sys.argv[1]
+      else:
+        prefix = 'it%s' % (int(time.time()))
+    self.repository = '/home/justinsb/juju/charms'
+    # service_name = 'it%s' % (int(time.time()))
+    self.service_name = prefix + '-proxy'
+    self.consumer_service_name = prefix + '-consumer'
+
+    self.jxaas_url = 'http://10.0.3.1:8080/xaas'
+    self.jxaas_tenant = 'abcdef'
+    self.jxaas_username = 'abcdef'
+    self.jxaas_password = 'abcdef'
+
+    self.jxaas = jujuxaas.client.Client(url=self.jxaas_url,
+                                        tenant=self.jxaas_tenant,
+                                        username=self.jxaas_username,
+                                        password=self.jxaas_password)
+
+
+  def init(self):
+    backend_main_service_name = 'u%s-%s-%s-%s' % (self.jxaas_tenant, self.bundle_type, self.service_name, self.bundle_type)
+    self.backend_main_service_name = backend_main_service_name
+
+  def run_test(self):
+    self.init()
+
+    self.create_proxy_service()
+    self.create_consumer_service()
+    self.create_consumer_relation()
+    self.wait_main_started()
+    relinfo = self.get_relation_properties()
+    self.wait_consumer_started()
+
+    self.check_service_state_consumer()
+
+    self.check_log()
+
+    self.change_properties()
+    self.test_scaling()
+
+    self.check_metrics()
+
+    self.cleanup()
+
+  def wait_main_started(self):
+    service_state = wait_service_started(self.service_name)
+    log.info("Proxy charm, all units started: %s", service_state)
+
+    wait_jxaas_started(self.jxaas, self.bundle_type, self.service_name)
+
+    main_service_state = wait_service_started(self.backend_main_service_name)
+    log.info("Main XaaS service started: %s", main_service_state)
+
+  def get_relation_properties(self):
+    relinfo = self.jxaas.get_relation_properties(self.bundle_type, self.service_name, self.juju_interface)
+    relinfo = relinfo['Properties']
+    log.info("JXaaS relation properties: %s", relinfo)
+
+    return relinfo
+
+  def wait_consumer_started(self):
+    consumer_service_state = wait_service_started(self.consumer_service_name)
+    log.info("Consumer charm, all units started: %s", consumer_service_state)
+
+  def create_proxy_service(self):
+    # status = juju_status()
+    # print status['services'].keys()
+
+    if not get_service_state(self.service_name):
+      config = {}
+      config['jxaas-url'] = self.jxaas_url
+      config['jxaas-tenant'] = self.jxaas_tenant
+      config['jxaas-user'] = self.jxaas_username
+      config['jxaas-secret'] = self.jxaas_password
+      juju_deploy_service(self.proxy_charm, self.service_name, self.repository, config)
+
+      # time.sleep(120)
+
+  def create_consumer_service(self):
+    if not get_service_state(self.consumer_service_name):
+      juju_deploy_service(self.consumer_charm, self.consumer_service_name, self.repository)
+
+  def create_consumer_relation(self):
+    raise Exception("Must implement create_consumer_relation")
+
+  def check_service_state_consumer(self):
+    log.warn("check_service_state_consumer not implemented")
+
+  def change_properties(self):
+    log.warn("change_properties not implemented")
+
+  def test_scaling(self):
+    log.warn("test_scaling not implemented")
+
+  def check_log(self):
+    logs = self.jxaas.get_log(self.bundle_type, self.service_name)
+    assert len(logs) > 0
+
+    # TODO: Verify a line from the log
+    # TODO: Verify that there are no lines from other services in the log
+
+  def check_metrics(self):
+    metrics = self.jxaas.get_metrics(self.bundle_type, self.service_name)
+    metrics = metrics['Metric']
+    print "Metrics length %s" % (len(metrics))
+    assert len(metrics) > 0
+
+    print metrics
+    # TODO: Verify some metrics from the log
+    # TODO: Verify there are no other metrics in the log
+
+  def cleanup(self):
+    self.jxaas.destroy_instance(self.bundle_type, self.service_name)
+
+    # TODO: Wait for service to disappear
+
+    # TODO: Really, destroying the service should do this...
+    juju_destroy_service(self.service_name)
+
+    juju_destroy_service(self.consumer_service_name)
